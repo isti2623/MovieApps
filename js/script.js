2875e71560591eda10e5b512dcad293d705acce9
isti2623 <istiaqueahmed2019.bd@gmail.com>
2021-08-16 00:28:02 +0600

    update

--- js/script.js
+++ js/script.js
@@ -12,21 +12,62 @@ const showMovieYear = document.getElementById('show-movie-year');
 
 const submitBtn = document.getElementById('submit-btn');
 
+
+
+names.addEventListener('keyup', () => {
+    var nameText = names.value
+    if (nameText != '') {
+        submitBtn.removeAttribute("disabled");
+
+
+    }
+
+
+
+});
+
 submitBtn.addEventListener('click', function () {
-    const nameText = names.value;
-    showMovieNames.innerText = nameText;
-    names.value = '';
 
-    const authorText = author.value;
-    showMovieAuthor.innerText = authorText;
-    author.value = '';
+    // const nameText = names.value;
+    // showMovieNames.innerText = nameText;
+    // names.value = '';
 
-    const desText = des.value;
-    showMovieDes.innerText = desText;
-    des.value = '';
+    // const authorText = author.value;
+    // showMovieAuthor.innerText = authorText;
+    // author.value = '';
 
-    const yearText = year.value;
-    showMovieYear.innerText = yearText;
-    year.value = '';
+    // const desText = des.value;
+    // showMovieDes.innerText = desText;
+    // des.value = '';
+
+    // const yearText = year.value;
+    // showMovieYear.innerText = yearText;
+    // year.value = '';
+
+    if (names != '' && author != '' && des != '' && year != '') {
+        const nameText = names.value;
+        showMovieNames.innerText = nameText;
+        names.value = '';
+
+        const authorText = author.value;
+        showMovieAuthor.innerText = authorText;
+        author.value = '';
+
+        const desText = des.value;
+        showMovieDes.innerText = desText;
+        des.value = '';
+
+        const yearText = year.value;
+        showMovieYear.innerText = yearText;
+        year.value = '';
+
+        document.getElementById('errorMsg').classList.remove('d-none');
+
+
+    }
+
+
+
+
 })
 
